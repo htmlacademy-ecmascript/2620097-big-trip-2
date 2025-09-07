f30d7ee10cd95edbaff548c2d25b009be74240e0
Alexnerud <a.ivanov23021989@gmail.com>
2025-09-07 23:05:43 +0300

    1.8 Добавляет board presenter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import NewSortView from './view/sort-view.js';
-import NewEditPointView from './view/edit-point-view.js';
+// import NewSortView from './view/sort-view.js';
+// import NewEditPointView from './view/edit-point-view.js';
 import FiltersView from './view/filters-view.js';
+// import TripListView from './view/trip-list-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
 import {render} from './render.js';
 
 
@@ -8,7 +10,14 @@ const siteHeaderElement = document.querySelector('.page-header');
 const tripHeaderElement = siteHeaderElement.querySelector('.trip-main');
 const pageMainElement = document.querySelector('.page-main');
 const mainContainerElement = pageMainElement.querySelector('.page-body__container');
+// const mainTripEventsElement = mainContainerElement.querySelector('.trip-events');
 
-render(new NewSortView(), tripHeaderElement);
-render(new NewEditPointView(), mainContainerElement);
+const boardPresenter = new BoardPresenter({boardContainer: mainContainerElement});
+// const mainTripEventsListElement = mainTripEventsElement.querySelector('.trip-events__list');
+
+// render(new NewSortView(), mainTripEventsElement);
+// render(new NewEditPointView(), mainContainerElement);
 render(new FiltersView(), tripHeaderElement);
+// render(new TripListView(), mainTripEventsElement);
+
+boardPresenter.init();
