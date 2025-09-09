4d6676052dc12ad34a77e8c10d8261af6f86f32a
Alexnerud <a.ivanov23021989@gmail.com>
2025-09-09 22:03:20 +0300

    1.9 Добавляет filtersComponent

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // import NewSortView from './view/sort-view.js';
 // import NewEditPointView from './view/edit-point-view.js';
-import FiltersView from './view/filters-view.js';
+// import FiltersView from './view/filters-view.js';
 // import TripListView from './view/trip-list-view.js';
-import BoardPresenter from './presenter/board-presenter.js';
-import {render} from './render.js';
 
+import BoardPresenter from './presenter/board-presenter.js';
+// import {render} from './render.js';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const tripHeaderElement = siteHeaderElement.querySelector('.trip-main');
@@ -12,12 +12,14 @@ const pageMainElement = document.querySelector('.page-main');
 const mainContainerElement = pageMainElement.querySelector('.page-body__container');
 // const mainTripEventsElement = mainContainerElement.querySelector('.trip-events');
 
-const boardPresenter = new BoardPresenter({boardContainer: mainContainerElement});
+const boardPresenter = new BoardPresenter({
+  boardContainer: mainContainerElement,
+  filtersContainer: tripHeaderElement,});
 // const mainTripEventsListElement = mainTripEventsElement.querySelector('.trip-events__list');
 
 // render(new NewSortView(), mainTripEventsElement);
 // render(new NewEditPointView(), mainContainerElement);
-render(new FiltersView(), tripHeaderElement);
+// render(new FiltersView(), tripHeaderElement);
 // render(new TripListView(), mainTripEventsElement);
 
 boardPresenter.init();
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,6 +1,7 @@
 import BoardView from '../view/bord-view.js';
 import TripListView from '../view/trip-list-view.js';
 import SortView from '../view/sort-view.js';
+import FiltersView from '../view/filters-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import PointEventView from '../view/point-event-view.js';
 import {render} from '../render.js';
@@ -8,12 +9,20 @@ import {render} from '../render.js';
 export default class BoardPresenter {
   boardComponent = new BoardView();
   tripListComponent = new TripListView();
+  filtersComponent = new FiltersView();
 
-  constructor({boardContainer}) {
+  constructor({boardContainer, filtersContainer}) {
     this.boardContainer = boardContainer;
+    this.filtersContainer = filtersContainer;
   }
 
   init() {
+
+    if (this.filtersContainer) {
+      render(this.filtersComponent, this.filtersContainer);
+    }
+
+
     render(this.boardComponent, this.boardContainer);
     render(new SortView(), this.boardComponent.getElement());
     render(this.tripListComponent, this.boardComponent.getElement());
